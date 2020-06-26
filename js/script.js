6b2ec0e2976df6f27633e6362233dec2dfe1af95
Tathiane <tathianesouza1@gmail.com>
2020-06-25 23:56:36 -0300

    Adds effect to my name

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
 function closeMenu() {
-    $('.close-menu').click(function () {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 1000);
-        $('.navbar-toggler').click(); // FAZ UM CLIQUE NO BOTAO QUE CHAMA O MENU PARA OCULTAR O MENU
-        return false;
-    }); 
+	$('.close-menu').click(function () {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1000);
+		$('.navbar-toggler').click(); // FAZ UM CLIQUE NO BOTAO QUE CHAMA O MENU PARA OCULTAR O MENU
+		return false;
+	});
 }
 
 
@@ -18,5 +17,16 @@ function goTop() {
 		} else {
 			$(".go-top").css("display", "none");
 		}
-	});	
+	});
 }
+
+function typeWriter(element) {
+	const text = element.innerHTML.split('');
+	element.innerHTML = '';
+	text.forEach((leter, i) => {
+		setTimeout(() => element.innerHTML += leter, 90 * i)
+	});
+}
+
+const title = document.querySelector('.tat');
+typeWriter(title);
